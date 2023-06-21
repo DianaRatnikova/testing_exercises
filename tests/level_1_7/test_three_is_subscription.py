8d2d5df93822671117442e7d1932f25d36bd3a27
DianaRatnikova <diana_r@bk.ru>
2023-06-21 18:35:57 +0300

    Улучшила форматирование и убрала лишние списки

--- tests/level_1_7/test_three_is_subscription.py
+++ tests/level_1_7/test_three_is_subscription.py
@@ -5,55 +5,58 @@ import decimal
 
 
 def test__is_subscription__short_history():
-    expense1 = Expense(amount=decimal.Decimal('10.8'), 
-                       currency=Currency("USD"), 
-                       card = BankCard(last_digits='234', owner="John"), 
-                       spent_in="green apple", 
-                       spent_at=datetime.datetime(2005, 1, 14),
-                       category=ExpenseCategory("SUPERMARKET"))
-    history_list = [expense1, expense1]
-    assert is_subscription(expense1, history_list) == False
+    expense1 = Expense(
+        amount=decimal.Decimal('10.8'), 
+        currency=Currency("USD"), 
+        card = BankCard(last_digits='234', owner="John"), 
+        spent_in="green apple", 
+        spent_at=datetime.datetime(2005, 1, 14),
+        category=ExpenseCategory("SUPERMARKET"))
+    assert is_subscription(expense1, [expense1, expense1]) == False
 
 
 def test__is_subscription__long_history():
-    expense1 = Expense(amount=decimal.Decimal('10.8'), 
-                       currency=Currency("USD"), 
-                       card = BankCard(last_digits='234', owner="John"), 
-                       spent_in="green apple", 
-                       spent_at=datetime.datetime(2005, 1, 14),
-                       category=ExpenseCategory("SUPERMARKET"))
-    history_list = [expense1, expense1, expense1, expense1, expense1]
-    assert is_subscription(expense1, history_list) == False
+    expense1 = Expense(
+        amount=decimal.Decimal('10.8'), 
+        currency=Currency("USD"),
+        card = BankCard(last_digits='234', owner="John"),
+        spent_in="green apple",
+        spent_at=datetime.datetime(2005, 1, 14),
+        category=ExpenseCategory("SUPERMARKET"))
+    assert is_subscription(expense1, [expense1, expense1, expense1, expense1, expense1]) == False
 
 
 def test__is_subscription__true():
-   expense1 = Expense(amount=decimal.Decimal('10.8'), 
-                       currency=Currency("USD"), 
-                       card = BankCard(last_digits='234', owner="John"), 
-                       spent_in="green apple", 
-                       spent_at=datetime.datetime(2005, 1, 14),
-                       category=ExpenseCategory("SUPERMARKET"))
+   expense1 = Expense(
+       amount=decimal.Decimal('10.8'),
+       currency=Currency("USD"),
+       card = BankCard(last_digits='234', owner="John"),
+       spent_in="green apple",
+       spent_at=datetime.datetime(2005, 1, 14),
+       category=ExpenseCategory("SUPERMARKET"))
    
-   expense1_1 = Expense(amount=decimal.Decimal('10.8'), 
-                   currency=Currency("USD"), 
-                   card = BankCard(last_digits='234', owner="John"), 
-                   spent_in="green apple", 
-                   spent_at=datetime.datetime(2005, 2, 14),
-                   category=ExpenseCategory("SUPERMARKET"))
+   expense1_1 = Expense(
+       amount=decimal.Decimal('10.8'),
+       currency=Currency("USD"),
+       card = BankCard(last_digits='234', owner="John"),
+       spent_in="green apple",
+       spent_at=datetime.datetime(2005, 2, 14),
+       category=ExpenseCategory("SUPERMARKET"))
    
-   expense1_2 = Expense(amount=decimal.Decimal('10.8'), 
-                   currency=Currency("USD"), 
-                   card = BankCard(last_digits='234', owner="John"), 
-                   spent_in="green apple", 
-                   spent_at=datetime.datetime(2005, 3, 14),
-                   category=ExpenseCategory("SUPERMARKET"))
+   expense1_2 = Expense(
+       amount=decimal.Decimal('10.8'),
+       currency=Currency("USD"),
+       card = BankCard(last_digits='234', owner="John"),
+       spent_in="green apple",
+       spent_at=datetime.datetime(2005, 3, 14),
+       category=ExpenseCategory("SUPERMARKET"))
    
-   expense1_3 = Expense(amount=decimal.Decimal('10.8'), 
-               currency=Currency("USD"), 
-               card = BankCard(last_digits='234', owner="John"), 
-               spent_in="green apple", 
-               spent_at=datetime.datetime(2005, 4, 14),
-               category=ExpenseCategory("SUPERMARKET"))
+   expense1_3 = Expense(
+       amount=decimal.Decimal('10.8'),
+       currency=Currency("USD"),
+       card = BankCard(last_digits='234', owner="John"),
+       spent_in="green apple",
+       spent_at=datetime.datetime(2005, 4, 14),
+       category=ExpenseCategory("SUPERMARKET"))
    
-   history_list = [expense1, expense1_1, expense1_2, expense1_3]
-   assert is_subscription(expense1, history_list) == True
+   assert is_subscription(expense1, [expense1, expense1_1, expense1_2, expense1_3]) == True
